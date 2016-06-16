60a17c09dd35e887d3f30e7f377674cbd0463053
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-06-16 10:38:33 +0300

    Update the tests

--- conflicts-test/Main.hs
+++ conflicts-test/Main.hs
@@ -31,19 +31,16 @@ main =
       do
         try (transaction connection1 Transactions.dropSchema) :: IO (Either SomeException ())
         transaction connection1 Transactions.createSchema
-        id1 <- session connection1 (Session.query 0 Queries.createAccount)
-        id2 <- session connection1 (Session.query 0 Queries.createAccount)
-        async1 <- Async.async (transaction connection1 (Transactions.transferTimes 200 id1 id2 1))
-        async2 <- Async.async (transaction connection2 (Transactions.transferTimes 200 id1 id2 1))
-        Async.wait async1
-        Async.wait async2
-        balance1 <- session connection1 (Session.query id1 Queries.getBalance)
-        balance2 <- session connection1 (Session.query id2 Queries.getBalance)
-        traceShowM balance1
-        traceShowM balance2
-        if balance1 == Just 400 && balance2 == Just (-400)
+        success <- fmap and (traverse runTest tests)
+        if success
           then exitSuccess
           else exitFailure
+      where
+        runTest test =
+          test connection1 connection2
+        tests =
+          [transactionsTest, transactionAndQueryTest]
+
 
 session connection session =
   Session.run session connection >>=
@@ -51,3 +48,37 @@ session connection session =
 
 transaction connection transaction =
   session connection (Transaction.run transaction Transaction.RepeatableRead Transaction.Write)
+
+
+type Test =
+  Connection.Connection -> Connection.Connection -> IO Bool
+
+transactionsTest :: Test
+transactionsTest connection1 connection2 =
+  do
+    id1 <- session connection1 (Session.query 0 Queries.createAccount)
+    id2 <- session connection1 (Session.query 0 Queries.createAccount)
+    async1 <- Async.async (replicateM_ 1000 (transaction connection1 (Transactions.transfer id1 id2 1)))
+    async2 <- Async.async (replicateM_ 1000 (transaction connection2 (Transactions.transfer id1 id2 1)))
+    Async.wait async1
+    Async.wait async2
+    balance1 <- session connection1 (Session.query id1 Queries.getBalance)
+    balance2 <- session connection1 (Session.query id2 Queries.getBalance)
+    traceShowM balance1
+    traceShowM balance2
+    return (balance1 == Just 2000 && balance2 == Just (-2000))
+
+transactionAndQueryTest :: Test
+transactionAndQueryTest connection1 connection2 =
+  do
+    id1 <- session connection1 (Session.query 0 Queries.createAccount)
+    id2 <- session connection1 (Session.query 0 Queries.createAccount)
+    async1 <- Async.async (transaction connection1 (Transactions.transferTimes 200 id1 id2 1))
+    async2 <- Async.async (session connection2 (replicateM_ 200 (Session.query (id1, 1) Queries.modifyBalance)))
+    Async.wait async1
+    Async.wait async2
+    balance1 <- session connection1 (Session.query id1 Queries.getBalance)
+    balance2 <- session connection1 (Session.query id2 Queries.getBalance)
+    traceShowM balance1
+    traceShowM balance2
+    return (balance1 == Just 400 && balance2 == Just (-200))
