8fca1c18eca033fd69e3c1dc00e5a926927a3c7c
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-07-01 12:05:22 +0300

    Add reexports

--- library/Hasql/Transaction/Session.hs
+++ library/Hasql/Transaction/Session.hs
@@ -2,13 +2,26 @@
 Extensions to Session.
 Can be used imported in the same namespace as @Hasql.Session@ without conflicts.
 -}
-module Hasql.Transaction.Session where
+module Hasql.Transaction.Session
+(
+  transaction,
+  {-* Reexports of non-conflicting types -}
+  {-|
+  Despite them not being rendered in the docs here we also reexport
+  the following types for you to specify lesser imports:
+  
+  * `Reexports.Transaction`
+  -}
+  module Reexports,
+)
+where
 
 import Hasql.Transaction.Prelude
 import Hasql.Transaction.Requisites.Model
 import Hasql.Transaction.Requisites.Sessions
 import Hasql.Session
 import qualified Hasql.Transaction.Transaction.Transaction as Transaction
+import qualified Hasql.Transaction.Transaction.Transaction as Reexports (Transaction)
 
 
 {-|
