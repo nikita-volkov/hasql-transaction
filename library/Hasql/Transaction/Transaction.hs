6581305b06736f37969eb74370ec8e8c4f839fa6
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-07-02 15:53:28 +0300

    Isolate an FAQ documentation section

--- library/Hasql/Transaction/Transaction.hs
+++ library/Hasql/Transaction/Transaction.hs
@@ -2,24 +2,15 @@ module Hasql.Transaction.Transaction
 (
   {-|
   Arrow DSL for composition of transactions with automated conflict resolution.
-
-  /Why does transaction have to be an arrow, why is monad or applicative not enough?/
-
-  Arrow allows us to determine the read mode and isolation level,
-  while composing transactions in such a way that one can depend on the result of the other.
-  A monadic interface wouldn't allow us to do the first, namely:
-  to compose the modes and levels. 
-  An applicative interface wouldn't allow the second:
-  to make a transaction depend on the result of the other.
-  For details see the docs on the `Transaction` type.
   -}
+  {-* Definitions -}
   Transaction,
   statement,
   sql,
   session,
   condemn,
   retry,
-  -- * Settings
+  {-* Settings -}
   Mode(..),
   Level(..),
   {-* Reexports -}
@@ -28,6 +19,19 @@ module Hasql.Transaction.Transaction
   
   * `Reexports.Session`
   * `Reexports.Statement`
+  -}
+  {-* FAQ -}
+  {-|
+  == Why does transaction have to be an arrow, why is monad or applicative not enough?
+
+  Arrow allows us to determine the read mode and isolation level,
+  while composing transactions in such a way that one can depend on the result of the other.
+  A monadic interface wouldn't allow us to do the first, namely:
+  to compose the modes and levels. 
+  
+  An applicative interface wouldn't allow the second:
+  to make a transaction depend on the result of the other.
+  For details see the docs on the `Transaction` type.
   -}
 )
 where
