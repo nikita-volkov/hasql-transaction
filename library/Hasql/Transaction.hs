d9b952be376f27d0ede8a1026422e4b6e1ab3ee0
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-09-05 10:21:18 +0300

    Merge into a single-module API

---
+++ library/Hasql/Transaction.hs
@@ -0,0 +1,47 @@
+{-|
+Arrow DSL for composition of transactions with automated conflict resolution.
+-}
+module Hasql.Transaction
+(
+  {-* Session -}
+  Session.transact,
+  {-* Transaction -}
+  Transaction,
+  statement,
+  sql,
+  session,
+  condemn,
+  retry,
+  {-* Settings -}
+  Mode(..),
+  Level(..),
+  {-* Reexports -}
+  {-|
+  This module also reexports the following types for you to require lesser imports:
+  
+  * `Reexports.Session`
+  * `Reexports.Statement`
+  -}
+  module Reexports,
+  {-* FAQ -}
+  {-|
+  == Why does transaction have to be an arrow, why is monad or applicative not enough?
+
+  Arrow allows us to determine the read mode and isolation level,
+  while composing transactions in such a way that one can depend on the result of the other.
+  
+  A monadic interface wouldn't allow us to do the first, namely:
+  to compose the modes and levels. 
+  
+  An applicative interface wouldn't allow the second:
+  to make a transaction depend on the result of the other.
+  For details see the docs on the `Transaction` type.
+  -}
+)
+where
+
+import Hasql.Transaction.Requisites.Model
+import Hasql.Transaction.Transaction
+import qualified Hasql.Session as Reexports (Session)
+import qualified Hasql.Statement as Reexports (Statement)
+import qualified Hasql.Transaction.Session as Session
--- library/Hasql/Transaction/Session.hs
+++ library/Hasql/Transaction/Session.hs
@@ -3,29 +3,19 @@ Extensions to Session.
 Can be used imported in the same namespace as @Hasql.Session@ without conflicts.
 -}
 module Hasql.Transaction.Session
-(
-  transaction,
-  {-* Reexports -}
-  {-|
-  This module also reexports the following types for you to require lesser imports:
-
-  * `Reexports.Transaction`
-  -}
-  module Reexports,
-)
 where
 
 import Hasql.Transaction.Prelude
 import Hasql.Transaction.Requisites.Model
 import Hasql.Transaction.Requisites.Sessions
 import Hasql.Session
-import qualified Hasql.Transaction.Transaction.Defs as Transaction
-import qualified Hasql.Transaction.Transaction.Defs as Reexports (Transaction)
+import qualified Hasql.Transaction.Transaction as Transaction
+import qualified Hasql.Transaction.Transaction as Reexports (Transaction)
 
 
 {-|
 Execute an alternating transaction arrow providing an input for it.
 -}
-transaction :: i -> Transaction.Transaction i o -> Session o
-transaction i (Transaction.Transaction mode level list) =
+transact :: i -> Transaction.Transaction i o -> Session o
+transact i (Transaction.Transaction mode level list) =
   inAlternatingTransaction mode level (fmap (\ fn -> runStateT (fn i) Uncondemned) list)
