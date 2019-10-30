d7069fdce7dfccd7c8a0b86f8a6d26a10899fba7
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-10-30 15:13:29 +0300

    Reimplement Transaction as Selective

--- library/Hasql/Transaction.hs
+++ library/Hasql/Transaction.hs
@@ -1,17 +1,16 @@
 {-|
-Arrow DSL for composition of transactions with automated conflict resolution.
+DSL for composition of transactions with automated conflict resolution.
 -}
 module Hasql.Transaction
 (
-  {-* Session -}
-  Session.transact,
+  {-* Sessions -}
+  transact,
   {-* Transaction -}
   Transaction,
   statement,
   sql,
   session,
   condemn,
-  retry,
   {-* Settings -}
   Mode(..),
   Level(..),
@@ -23,25 +22,119 @@ module Hasql.Transaction
   * `Reexports.Statement`
   -}
   module Reexports,
-  {-* FAQ -}
-  {-|
-  == Why does transaction have to be an arrow, why is monad or applicative not enough?
-
-  Arrow allows us to determine the read mode and isolation level,
-  while composing transactions in such a way that one can depend on the result of the other.
-  
-  A monadic interface wouldn't allow us to do the first, namely:
-  to compose the modes and levels. 
-  
-  An applicative interface wouldn't allow the second:
-  to make a transaction depend on the result of the other.
-  For details see the docs on the `Transaction` type.
-  -}
 )
 where
 
+import Hasql.Transaction.Prelude
 import Hasql.Transaction.Requisites.Model
-import Hasql.Transaction.Transaction
 import qualified Hasql.Session as Reexports (Session)
 import qualified Hasql.Statement as Reexports (Statement)
-import qualified Hasql.Transaction.Session as Session
+import qualified Hasql.Session as Session
+import qualified Hasql.Statement as Statement
+import qualified Hasql.Transaction.Requisites.Sessions as Sessions
+
+
+{-|
+Execute transaction in session.
+-}
+transact :: Transaction a -> Session.Session a
+transact (Transaction mode level list) =
+  Sessions.inAlternatingTransaction mode level $
+  fmap (flip runStateT Uncondemned) list
+
+
+{-|
+Composable transaction providing for automated conflict resolution.
+
+Mode and level is associated with the transaction,
+which makes them participate in composition.
+In a composed transaction they become the strictest of the ones
+associated with the transactions that constitute it.
+This allows you to safely compose transactions with different
+ACID guarantees.
+
+It cannot have an instance of Monad,
+because it makes it impossible to implement composition of
+mode and level associated with consituent transactions.
+It still however is possible to compose transactions
+in such a way that the result of a transaction is used in the next one
+using the recently discovered `Selective` interface.
+
+Supports alternative branching,
+where the alternative gets executed in case of a transaction conflict.
+-}
+data Transaction a = Transaction Mode Level [StateT Condemnation Session.Session a]
+
+deriving instance Functor Transaction
+
+instance Applicative Transaction where
+  pure = Transaction minBound minBound . pure . pure
+  (<*>) (Transaction mode1 level1 list1) (Transaction mode2 level2 list2) =
+    Transaction (max mode1 mode2) (max level1 level2) ((<*>) <$> list1 <*> list2)
+
+instance Selective Transaction where
+  select (Transaction mode1 level1 list1) (Transaction mode2 level2 list2) =
+    Transaction (max mode1 mode2) (max level1 level2) (select <$> list1 <*> list2)
+
+instance Alternative Transaction where
+  empty = Transaction minBound minBound []
+  (<|>) (Transaction mode1 level1 list1) (Transaction mode2 level2 list2) =
+    Transaction (max mode1 mode2) (max level1 level2) (list1 <> list2)
+
+{-|
+Execute a single statement under mode and level.
+
+__Warning:__ The statement must not be transaction-related
+like @BEGIN@, @COMMIT@ or @ABORT@, otherwise you'll break the abstraction.
+-}
+statement :: Mode -> Level -> Statement.Statement i o -> i -> Transaction o
+statement mode level statement i = session mode level $ Session.statement i statement
+
+{-|
+Execute a possibly multistatement SQL string under a mode and level.
+SQL strings cannot be dynamically parameterized or produce a result.
+
+__Warning:__ SQL must not be transaction-related
+like @BEGIN@, @COMMIT@ or @ABORT@, otherwise you'll break the abstraction.
+-}
+sql :: Mode -> Level -> ByteString -> Transaction ()
+sql mode level sql = session mode level $ Session.sql sql
+
+{-|
+Execute a composition of statements under the same mode and level.
+
+__Warning:__
+
+1. You must know that it is possible to break the abstraction,
+if you execute statements such as @BEGIN@ inside of the session.
+
+1. For the same reason you cannot execute other transactions inside of that session.
+
+1. You must beware that in case of conflicts any IO code that you may lift
+into session will get executed multiple times.
+This is the way the automatic conflict resolution works:
+the transaction gets retried, when a conflict arises.
+So be cautious about doing any mutations or rocket launches in that IO!
+Simply pinging for things such as current time is totally fine though.
+Still it's not recommended because it's often a symptom of bad application design.
+
+Due to the mentioned it's highly advised to keep all the session code
+inside of the definition of a transaction.
+Thus you'll be guaranteed to have control over what's going on inside of the
+executed session and it will not be possible for this code
+to be affected by any outside changes or used elsewhere.
+-}
+session :: Mode -> Level -> Session.Session a -> Transaction a
+session mode level session = Transaction mode level [lift session]
+
+{-|
+Cause transaction to eventually roll back.
+
+This allows to perform some transactional actions,
+collecting their results, and decide,
+whether to commit the introduced changes to the DB
+based on those results,
+as well as emit those results outside of the transaction.
+-}
+condemn :: Transaction ()
+condemn = Transaction minBound minBound [put Condemned]
