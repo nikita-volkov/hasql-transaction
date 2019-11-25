be077c0c5fe9978b89b35a4c8d03491cffcea233
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-11-25 19:29:14 +0300

    Rewrite sessions to isolate handling of errors in transaction body from its commitment

--- library/Hasql/Transaction/Private/Sessions.hs
+++ library/Hasql/Transaction/Private/Sessions.hs
@@ -3,8 +3,8 @@ where
 
 import Hasql.Transaction.Private.Prelude
 import Hasql.Transaction.Private.Model
-import qualified Hasql.Session as A
-import qualified Hasql.Transaction.Private.Statements as B
+import Hasql.Session
+import qualified Hasql.Transaction.Private.Statements as Statements
 
 
 {-
@@ -12,23 +12,32 @@ We may want to
 do one transaction retry in case of the 23505 error, and fail if an identical
 error is seen.
 -}
-inRetryingTransaction :: IsolationLevel -> Mode -> A.Session (a, Bool) -> A.Session a
-inRetryingTransaction isolation mode session =
-  fix $ \recur -> catchError normal (onError recur)
-  where
-    normal =
-      do
-        A.statement () (B.beginTransaction isolation mode)
-        (result, commit) <- session
-        if commit
-          then A.statement () B.commitTransaction
-          else A.statement () B.abortTransaction
-        return result
-    onError continue error =
-      do
-        A.statement () B.abortTransaction
-        case error of
-          A.QueryError _ _ (A.ResultError (A.ServerError "40001" _ _ _)) ->
-            continue
-          _ ->
-            throwError error
+inRetryingTransaction :: IsolationLevel -> Mode -> Session (a, Bool) -> Session a
+inRetryingTransaction level mode session =
+  fix $ \ retry -> do
+    attemptRes <- tryTransaction level mode session
+    case attemptRes of
+      Just a -> return a
+      Nothing -> retry
+
+tryTransaction :: IsolationLevel -> Mode -> Session (a, Bool) -> Session (Maybe a)
+tryTransaction level mode body = do
+
+  statement () (Statements.beginTransaction level mode)
+
+  bodyRes <- catchError (fmap Just body) $ \ error -> do
+    statement () Statements.abortTransaction
+    handleTransactionError error $ return Nothing
+
+  case bodyRes of
+    Just (res, commit) -> catchError (commitOrAbort commit $> Just res) $ \ error -> do
+      handleTransactionError error $ return Nothing
+    Nothing -> return Nothing
+
+commitOrAbort commit = if commit
+  then statement () Statements.commitTransaction
+  else statement () Statements.abortTransaction
+
+handleTransactionError error onTransactionError = case error of
+  QueryError _ _ (ResultError (ServerError "40001" _ _ _)) -> onTransactionError
+  error -> throwError error
